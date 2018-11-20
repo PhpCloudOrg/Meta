f48b7c72218bdcc3a328baca29bf092e341a2a97
PhpCloud.org <core@phpcloud.org>
2018-11-20 10:15:24 +0100

    Cover exception propagation in code style fixer check with test

--- test/src/CodeStyleFixerQualityCheckTest.php
+++ test/src/CodeStyleFixerQualityCheckTest.php
@@ -17,6 +17,7 @@ use PhpCloudOrg\Meta\CodeRepository\CodeRepositoryInterface;
 use PhpCloudOrg\Meta\CommandRunner\CommandRunnerInterface;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class CodeStyleFixerQualityCheckTest extends TestCase
 {
@@ -62,6 +63,55 @@ class CodeStyleFixerQualityCheckTest extends TestCase
                 'src/Authentication.php',
                 'test/bootstrap.php',
                 'test/src/Authentication.php',
+            ]
+        );
+    }
+
+    /**
+     * @expectedException \PhpCloudOrg\Meta\CodeQualityChecker\QualityCheck\CheckException
+     * @expectedExceptionMessage src/stdClass.php
+     */
+    public function testWillPropagateFailureException()
+    {
+        /** @var MockObject|ProcessFailedException $process_failed_exception */
+        $process_failed_exception = $this->createMock(ProcessFailedException::class);
+
+        /** @var MockObject|CommandRunnerInterface $command_runner */
+        $command_runner = $this->createMock(CommandRunnerInterface::class);
+        $command_runner
+            ->expects($this->once())
+            ->method('runCommand')
+            ->willThrowException($process_failed_exception);
+
+        /** @var MockObject|CodeRepositoryInterface $repository */
+        $repository = $this->createMock(CodeRepositoryInterface::class);
+        $repository
+            ->expects($this->once())
+            ->method('fileExists')
+            ->willReturn(true);
+
+        /** @var MockObject|FileSignatureResolverInterface $file_signature_resolver */
+        $file_signature_resolver = $this->createMock(FileSignatureResolverInterface::class);
+        $file_signature_resolver
+            ->expects($this->once())
+            ->method('getSignature')
+            ->willReturn('file-sig');
+
+        $check = new CodeStyleFixerQualityCheck(
+            $repository,
+            $command_runner,
+            $file_signature_resolver,
+            'php-cs-fixer',
+            '.php_cs.php',
+            null,
+            new FilePathMatcher('src', 'php'),
+            new FilePathMatcher('test/src', 'php')
+        );
+
+        $check->check(
+            __DIR__,
+            [
+                'src/stdClass.php',
             ]
         );
     }
